fb91c33f05730c4e69f083ae81af204f347bea6a
Sabri.GASMI <glad77fr@yahoo.fr>
2019-06-10 09:41:00 +0200

    1er version s

--- Split_Data.py
+++ Split_Data.py
@@ -3,6 +3,7 @@ import re
 import glob
 import xlsxwriter
 import os
+from Correct_typing import correct_typing
 
 
 staff = pd.read_csv(r"C:\Users\Sabri.GASMI\Desktop\ALDI\ALDI_ABLIS_STAFF_CLAIRE.csv",encoding="ansi", sep=";",decimal=',')
@@ -35,6 +36,7 @@ def split(file, dataframe_ref):
     file_name = file_name[::-1]
 
     file = pd.read_csv(file, encoding="ansi", sep= ";")
+    correct_typing(file)
     print(file.dtypes)
 
     file_staff = file[file['Matricule'].isin(dataframe_ref)]
@@ -43,7 +45,6 @@ def split(file, dataframe_ref):
 
     target_folder = file_rep[:-(len(file_name)+4)] + "\\Split\\"
 
-    print(target_folder)
 
     if os.path.isdir(target_folder):
         pass
@@ -54,11 +55,10 @@ def split(file, dataframe_ref):
     file_staff.to_excel(writer, sheet_name='Feuil1',index=False)
     writer.save()
 
-    file_staff.to_excel(target_folder + file_name + "_STAFF2.xlsx")
-    writer = pd.ExcelWriter(target_folder + file_name + "_NON_STAFF.xlsx", engine='xlsxwriter')
+    writer = pd.ExcelWriter(target_folder + file_name + "_NON_STAFF.xlsx", engine='xlsxwriter',date_format='DD-MM-YYYY')
     file_non_staff.to_excel(writer, sheet_name='Feuil1',index=False)
     writer.save()
-    print(file_staff)
+
 
 split(r"C:\Users\Sabri.GASMI\Desktop\ALDI\ALDI_ABLIS_STAFF_BASES ASSUJETTIES 042019.csv",dataframe_ref)
 
--- Test.py
+++ Test.py
@@ -2,3 +2,8 @@ import re
 r = re.compile(r"^\d*[.,]\d*$")
 if r.match('1,32'):
     print ('it matches!')
+
+j = re.compile(r'^\d*$')
+
+if j.match('70'):
+    print("GOOD")
